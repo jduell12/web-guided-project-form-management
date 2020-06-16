96cade4b3a5f4db73c969c284d4ff0e23f52cf59
Jessica <duellje@oregonstate.edu>
2020-06-16 13:44:10 -0400

    task 9

--- src/components/App.js
+++ src/components/App.js
@@ -24,6 +24,7 @@ const initialFormValues = {
 
 export default function App() {
   const [friends, setFriends] = useState(initialFriendsList)
+  const [err, setErr] = useState('')
 
   // 🔥 STEP 1 - WE NEED STATE TO HOLD ALL VALUES OF THE FORM!
   const [formValues, setFormValues] = useState(initialFormValues) // fix this using the state hook
@@ -33,9 +34,8 @@ export default function App() {
     // which can change the state of inputs of type text
 
     // a) pull the name of the input from the event object
-    let name = evt.target.name;
     // b) pull the value of the input from the event object
-    let value = evt.target.value;
+    const {name, value} = evt.target;
     // c) set a new state for the whole form
     setFormValues({...formValues, [name]:value})
   }
@@ -46,18 +46,25 @@ export default function App() {
     // a) don't allow the browser to reload!
     evt.preventDefault();
     // b) don't allow the submission, if any of the formValues is empty!
+    if(!formValues.username || !formValues.email || !formValues.role){
+      setErr('You need to fill out the form!')
+      return
+    }
     // c) make a new friend object
     //    set up the new friend with the correct attributes
     //    using the information inside the state of the form
+    const newFriend = {...formValues, id: uuid()}
     // d) update the list of friends in state with the new friend
+    setFriends(friends => [newFriend, ...friends]);
     // e) optionally clear the form
+    setFormValues(initialFormValues);
     
   }
 
   return (
     <div className='container'>
       <header><h1>Friends App</h1></header>
-
+      <span style={{color: 'red'}}>{err}</span>
       <FriendForm
         // 🔥 STEP 2 - The form component needs its props.
         //    Check implementation of FriendForm
